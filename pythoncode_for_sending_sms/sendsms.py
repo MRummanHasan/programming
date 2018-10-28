4665c469f13662eafc7dddc22ec0d00c76cbf0d1
Muhammad Rumman Hasan Khan <mrummanhasan@gmail.com>
2018-10-28 21:18:48 +0500

    Update sendsms.py

--- pythoncode_for_sending_sms/sendsms.py
+++ pythoncode_for_sending_sms/sendsms.py
@@ -9,8 +9,8 @@ from dateutil import parser #third party library
 # Input: PatietnContactNo, DocName, Timings
 # output: nothing
 def sendsms(pContactNo, DocName, Timings):
-    account_sid = 'AC0b4008bfc4afa66dcc6ef1579fb7f808'
-    auth_token = 'f39367feb8e6e4cc9f58085d215a1303'
+    account_sid = ''
+    auth_token = ''
 
     myPhone = '+92-1234567890' #enter your number, on which you want to send SMS(msg)
     TwilioNumber = '+123456778' #Your twilio number, issued by Twilio
@@ -20,4 +20,4 @@ def sendsms(pContactNo, DocName, Timings):
     client.messages.create(
         to=myPhone,
         from_=TwilioNumber,
-        body='Hello Hactoberfest,\nThis is a contribution towards opensource \n Happy coding :)')
+        body='Hello Hactoberfest,\nThis is a contribution towards opensource \n Happy coding :p')
